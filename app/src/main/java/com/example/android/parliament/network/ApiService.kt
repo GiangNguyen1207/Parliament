fb251d8f835149829305ddcdfcb8ea02f78aca66
giang <nguyengiang.nchg@gmail.com>
2021-09-25 13:42:05 +0300

    implement fetch list of parliament members

---
+++ app/src/main/java/com/example/android/parliament/network/ApiService.kt
@@ -0,0 +1,30 @@
+package com.example.android.parliament.network
+
+import com.example.android.parliament.data.ParliamentMember
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+
+private const val BASE_URL = "https://users.metropolia.fi/~peterh/"
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+interface ApiService {
+    @GET("mps.json")
+    suspend fun fetchAllMembers(): List<ParliamentMember>
+}
+
+object MembersApi {
+    val retrofitService: ApiService by lazy {
+        retrofit.create(ApiService::class.java)
+    }
+}
--- app/src/main/java/com/example/android/parliament/ui/AppViewModel.kt
+++ app/src/main/java/com/example/android/parliament/ui/AppViewModel.kt
@@ -1,15 +1,35 @@
 package com.example.android.parliament.ui
 
 import android.content.Context
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.android.parliament.data.AppDatabase
 import com.example.android.parliament.data.ParliamentMemberRepository
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
 class AppViewModel(context: Context) : ViewModel() {
     private val repository: ParliamentMemberRepository
+    private val response = MutableLiveData<String>()
 
     init {
         val memberDao = AppDatabase.getDatabase(context).parliamentMemberDao()
         repository = ParliamentMemberRepository(memberDao)
+        fetchParliamentMembers()
+    }
+
+    private fun fetchParliamentMembers() {
+        viewModelScope.launch {
+            try {
+                val allMembers = repository.fetchAllMembers()
+                for (member in allMembers) {
+                    repository.insertMember(member)
+                }
+
+            } catch(error: Exception) {
+                response.value = "Failure + ${error.message}"
+            }
+        }
     }
 }
